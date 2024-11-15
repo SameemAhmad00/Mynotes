3b975db8b426ed37732c4e1fdf939e7bcac6dedd
SameemAhmad00 <118016523+SameemAhmad00@users.noreply.github.com>
2024-11-15 19:24:12 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function HSC() {
+// Redirect to the new page
+window.location.href = "/12th Science/12thscience.html";
+}
+
+function DPharmacy() {
+// Redirect to the new page
+window.location.href = "/D-Pharmacy/D-Pharmacy.html";
+}
