fc1af01e82a83936f43bff26744215bf4a375427
SameemAhmad00 <118016523+SameemAhmad00@users.noreply.github.com>
2025-07-23 21:10:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function HSC() {
 // Redirect to the new page
-window.location.href = "Mynotes/12th Science/12thscience.html";
+window.location.href = "/12th Science/12thscience.html";
 }
 
 function DPharmacy() {
 // Redirect to the new page
-window.location.href = "Mynotes/D-Pharmacy/D-Pharmacy.html";
+window.location.href = "/D-Pharmacy/D-Pharmacy.html";
+}
+
+function Notes() {
+    // Redirect to the new page
+window.location.href = "/Notes/Notes.html";
 }
